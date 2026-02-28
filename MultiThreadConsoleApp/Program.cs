c1a10ca3e44fc08718528426d2c879e4e4ea5019
Макс <maxm4496yandex.ru>
2026-02-28 15:16:49 +0300

    Добавлен класс View для представления экрана в консоли

--- MultiThreadConsoleApp/Program.cs
+++ MultiThreadConsoleApp/Program.cs
@@ -5,46 +5,7 @@ using System.Diagnostics;
 using System.Text;
 
 using MultiThreadConsoleApp;
-/*
-ThreadSafeQueueData data = new ThreadSafeQueueData();
-long last_length = 0;
-object locker = new object();
 
-void KeyBoardReader()
-{
-    while (true)
-    {
-
-        ConsoleKeyInfo consoleKey = Console.ReadKey(true);
-
-         Debug.WriteLine($"Key:{consoleKey.Key.ToString()}\tKeyChar:{consoleKey.KeyChar}\tModifires:{consoleKey.Modifiers}");
-         data.Push(consoleKey.KeyChar);
-        
-    }
-}
-
-void ScreenBuilder()
-{
-    
-    while (true) {
-
-        if (data.CheckLastAction())
-        { 
-            Console.Write(data.Pop());
-        }
-    }
-}
-
-Console.InputEncoding = Encoding.UTF8;
-Console.OutputEncoding = Encoding.UTF8;
-
-
-Thread KeySpectator = new Thread(KeyBoardReader);
-Thread ScreenPainter = new Thread(ScreenBuilder);
-
-KeySpectator.Start();
-ScreenPainter.Start();
-*/
 
 Manager.Run();
 
@@ -52,20 +13,26 @@ static class Manager
 {
     private static ThreadSafeQueueData data = new ThreadSafeQueueData();
     private static ThreadSafeQueueCommands commands = new ThreadSafeQueueCommands();
-
+    private static SimpleView screen = new SimpleView("Simple editor", ["enter - new line","del - del line" ]);
     private static void KeyBoardReader()
     {
         while (true)
         {
-
             ConsoleKeyInfo consoleKey = Console.ReadKey(true);
-
             Debug.WriteLine($"Key:{consoleKey.Key.ToString()}\tKeyChar:{consoleKey.KeyChar}\tModifires:{consoleKey.Modifiers}");
 
-            //data.Push(consoleKey.KeyChar);
+
             switch (consoleKey.Key,consoleKey.Modifiers) {
-                case (ConsoleKey.Enter, ConsoleModifiers.None):{data.Push('\n');break; }
-                default: { if (Char.IsLetter(consoleKey.KeyChar)) { data.Push(consoleKey.KeyChar); } ;break; }
+                case (ConsoleKey.Enter, ConsoleModifiers.None):{ data.Push('\n'); screen.AddChar('\n'); ;break; }
+                case (ConsoleKey.Delete, ConsoleModifiers.None): { Console.Clear(); break; }
+                /*case (ConsoleKey.Backspace, ConsoleModifiers.None):
+                {
+                        Console.Clear();
+                        
+                        data.PopDel();
+                        break;
+                }*/
+                default: { if (Char.IsLetter(consoleKey.KeyChar)) { data.Push(consoleKey.KeyChar); screen.AddChar(consoleKey.KeyChar); } ;break; }
             }
             
         }
@@ -76,11 +43,15 @@ static class Manager
 
         while (true)
         {
-
-            if (data.CheckLastAction())
+            if (screen.ScreenChanged())
+            {
+                Console.Clear();
+                Console.WriteLine(screen.GetScreen());
+            }
+            /*if (data.CheckLastAction())
             {
                 Console.Write(data.Pop());
-            }
+            }*/
         }
     }
 
---
+++ MultiThreadConsoleApp/Views.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MultiThreadConsoleApp
+{
+    public class SimpleView
+    {
+        protected string title;
+        protected List<String> functions;
+        protected List<String> text;
+        protected object AtomicFlagChange = false;
+        protected object Locker = new object();
+
+        public SimpleView(string title, List<String> functions)
+        {
+            this.title = title;
+            this.functions = functions;
+            text = new List<String>() {""};
+        }
+
+        public string GetScreen()
+        {
+            Interlocked.Exchange(ref this.AtomicFlagChange, false);
+
+            lock (Locker)
+            {
+                string str = $"--=={title}==--\n";
+                foreach (String function in functions)
+                {
+                    str += $"\t{function}";
+                }
+                str += "\n\n";
+
+                foreach (String line in text)
+                {
+                    str += line;
+                }
+                return str;
+
+            }
+        }
+
+        public bool ScreenChanged() { return (AtomicFlagChange as bool?)??false; }
+
+        public void AddChar(char ch) {
+
+
+            lock (Locker)
+            {
+                if (ch == '\n')
+                {
+                    text.Last().Append('\n');
+                    text.Add("");
+                }
+                else
+                {
+                    text.Last().Append(ch);
+                }
+            }
+
+            Interlocked.Exchange(ref this.AtomicFlagChange, true);
+
+        }
+
+        public void RemoveChar()
+        {
+
+            lock (Locker)
+            {
+                if (text.Count > 0)
+                {
+
+                    if (text.Last().Length > 0)
+                    {
+                        text[text.Count - 1] = text.Last().Remove(text.Last().Length - 1);
+                    }
+                    else
+                    {
+                        text.RemoveAt(text.Count - 1);
+                    }
+                }
+            }
+
+            Interlocked.Exchange(ref this.AtomicFlagChange, true);
+
+        }
+
+        public void RemoveLine()
+        {
+
+            lock (Locker) {
+                if (text.Count > 0)
+                {
+                    text.RemoveAt(text.Count - 1);
+
+                }
+            }
+
+            Interlocked.Exchange(ref this.AtomicFlagChange, true);
+
+        }
+
+    }
+}
