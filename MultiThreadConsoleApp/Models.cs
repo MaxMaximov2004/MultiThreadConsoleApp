574bf353aff60e4df2ec2f6651950daaf0ac8c24
Макс <@DESKTOP-M6KTJO9>
2026-02-28 12:03:36 +0300

    Добавьте файлы проекта.

---
+++ MultiThreadConsoleApp/Models.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MultiThreadConsoleApp
+{
+    public interface SafeQueue;
+    public class ThreadSafeQueueData:SafeQueue
+    {
+        private Mutex? mut;
+        private object? locker;
+        private Queue<char>? data;
+        private bool Action = false;
+        public ThreadSafeQueueData()
+        {
+            mut = new Mutex();
+            data = new Queue<char>();
+            locker = new object();
+        }
+
+        public void Push(char ch)
+        {
+            lock (locker)
+            {
+                Action = true;
+                data.Enqueue(ch);
+            }
+        }
+
+        public char Pop()
+        {
+            lock (locker)
+            {
+                char last = data.LastOrDefault('!');
+                return last;
+            }
+        }
+        
+        public void PopDel()
+        {
+            lock (locker)
+            {
+                Action = true;
+                if (data.Count > 0)
+                {
+                    data.Dequeue();
+                }
+            }
+        }
+
+        public bool CheckLastAction()
+        {
+            lock (locker)
+            {
+                bool Flag = false;
+                if (Action)
+                {
+                    Flag = true;
+                    Action = false;
+                }
+                return Flag;
+            }
+        }
+    }
+
+
+    enum Commands:byte { Nothing,Del,NewLine,RollBack,RollForward,Exit }
+
+    class ThreadSafeQueueCommands:SafeQueue
+    {
+        private Mutex? mut;
+        private object? locker;
+        private Queue<Commands>? commands;
+        private bool Action = false;
+        public ThreadSafeQueueCommands()
+        {
+            mut = new Mutex();
+            commands = new Queue<Commands>();
+            locker = new object();
+        }
+
+
+        public void Push(Commands ch)
+        {
+            lock (locker)
+            {
+                Action = true;
+                commands.Enqueue(ch);
+            }
+        }
+
+        public Commands Pop()
+        {
+            lock (locker)
+            {
+                Commands last = commands.LastOrDefault(Commands.Nothing);
+                return last;
+            }
+        }
+
+        public void PopDel()
+        {
+            lock (locker)
+            {
+                Action = true;
+                if (commands.Count > 0)
+                {
+                    commands.Dequeue();
+                }
+            }
+        }
+
+        public bool CheckLastAction()
+        {
+            lock (locker)
+            {
+                bool Flag = false;
+                if (Action)
+                {
+                    Flag = true;
+                    Action = false;
+                }
+                return Flag;
+            }
+        }
+    }
+
+}
---
+++ MultiThreadConsoleApp/Program.cs
@@ -0,0 +1,101 @@
+﻿using System.Threading;
+
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Text;
+
+using MultiThreadConsoleApp;
+/*
+ThreadSafeQueueData data = new ThreadSafeQueueData();
+long last_length = 0;
+object locker = new object();
+
+void KeyBoardReader()
+{
+    while (true)
+    {
+
+        ConsoleKeyInfo consoleKey = Console.ReadKey(true);
+
+         Debug.WriteLine($"Key:{consoleKey.Key.ToString()}\tKeyChar:{consoleKey.KeyChar}\tModifires:{consoleKey.Modifiers}");
+         data.Push(consoleKey.KeyChar);
+        
+    }
+}
+
+void ScreenBuilder()
+{
+    
+    while (true) {
+
+        if (data.CheckLastAction())
+        { 
+            Console.Write(data.Pop());
+        }
+    }
+}
+
+Console.InputEncoding = Encoding.UTF8;
+Console.OutputEncoding = Encoding.UTF8;
+
+
+Thread KeySpectator = new Thread(KeyBoardReader);
+Thread ScreenPainter = new Thread(ScreenBuilder);
+
+KeySpectator.Start();
+ScreenPainter.Start();
+*/
+
+Manager.Run();
+
+static class Manager
+{
+    private static ThreadSafeQueueData data = new ThreadSafeQueueData();
+    private static ThreadSafeQueueCommands commands = new ThreadSafeQueueCommands();
+
+    private static void KeyBoardReader()
+    {
+        while (true)
+        {
+
+            ConsoleKeyInfo consoleKey = Console.ReadKey(true);
+
+            Debug.WriteLine($"Key:{consoleKey.Key.ToString()}\tKeyChar:{consoleKey.KeyChar}\tModifires:{consoleKey.Modifiers}");
+
+            //data.Push(consoleKey.KeyChar);
+            switch (consoleKey.Key,consoleKey.Modifiers) {
+                case (ConsoleKey.Enter, ConsoleModifiers.None):{data.Push('\n');break; }
+                default: { if (Char.IsLetter(consoleKey.KeyChar)) { data.Push(consoleKey.KeyChar); } ;break; }
+            }
+            
+        }
+    }
+
+    private static void ScreenBuilder()
+    {
+
+        while (true)
+        {
+
+            if (data.CheckLastAction())
+            {
+                Console.Write(data.Pop());
+            }
+        }
+    }
+
+    public static void Run()
+    {
+        Console.InputEncoding = Encoding.UTF8;
+        Console.OutputEncoding = Encoding.UTF8;
+
+
+        Thread KeySpectator = new Thread(KeyBoardReader);
+        Thread ScreenPainter = new Thread(ScreenBuilder);
+
+        KeySpectator.Start();
+        ScreenPainter.Start();
+
+    }
+
+}
